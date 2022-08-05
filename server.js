c65be5e06864552cfe3126e86ac117391eaa12ae
Jose Mario <juholiver3@gmail.com>
2022-08-05 16:31:59 -0300

    JsonServe

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const jsonServer = require('json-server');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
-app.use(express.static(__dirname + '/dist/app2'))
+
+app.use('/api', jsonServer.router('banco-de-dados.json'));
+app.use(express.static(__dirname + '/dist/app2'));
+
 
 app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/dist/app2/index.html')
